d2b2671296391a3e3b34969289e255a34da1bccc
Jonathan Min <39451363+min-j@users.noreply.github.com>
2018-10-11 16:31:44 -0400

    Uploading Camera and Player Control codes

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    CharacterController characterController;
+
+    public float speed;
+    public float jumpSpeed;
+    public float gravity = 20f;
+
+    private Vector3 moveDirection = Vector3.zero;
+ 
+	// Use this for initialization
+	void Start () {
+        characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime); 
+	}
+}
