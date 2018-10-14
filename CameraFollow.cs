baf500e67a355bbda07a666c857ae646c0d7fb32
Jonathan Min <39451363+min-j@users.noreply.github.com>
2018-10-14 12:33:01 -0400

    Uploading CameraFollow
    CameraFollow is basically CameraControl but renamed for accuracy and implemented a x-axis rotation around the specified character. I will look into y-axis rotation someday.

---
+++ CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public float speed;
+    public GameObject player;
+    public bool rotate = true;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (rotate) {
+            Quaternion turn = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * speed, Vector3.up);
+            offset = turn * offset;
+        }
+
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform);
+	}
+}
